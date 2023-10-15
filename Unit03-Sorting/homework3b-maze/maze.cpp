d26bff3d88b86d2c12fea478ac6483758c3f00f1
Anna Bodily <annalallen6@gmail.com>
2023-10-15 13:08:35 -0600

    Adding old code

--- Unit03-Sorting/homework3b-maze/maze.cpp
+++ Unit03-Sorting/homework3b-maze/maze.cpp
@@ -1,10 +1,92 @@
 #include <iostream>
-#include <string>
-
+#include <fstream>
+#include <vector>
 #include "Grid.h"
 
+// Function to solve the maze using Depth-First Search
+bool solveMaze(Grid& maze, int row, int column, int level) {
+    // Check if we have reached the end of the maze
+    if (row == maze.height() - 1 && column == maze.width() - 1 && level == maze.depth() - 1) {
+        return true;  // Found a solution
+    }
+
+    // Mark the current cell as visited
+    maze.at(row, column, level) = 2;
+
+    // Define possible moves (up, down, left, right, in, out)
+    int moves[6][3] = {{-1, 0, 0}, {1, 0, 0}, {0, -1, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1}};
+
+    for (int i = 0; i < 6; i++) {
+        int newRow = row + moves[i][0];
+        int newColumn = column + moves[i][1];
+        int newLevel = level + moves[i][2];
+
+        // Check if the new move is within bounds and leads to a passable space
+        if (newRow >= 0 && newRow < maze.height() &&
+            newColumn >= 0 && newColumn < maze.width() &&
+            newLevel >= 0 && newLevel < maze.depth() &&
+            maze.at(newRow, newColumn, newLevel) == 1) {
+            // Updated distance check
+            int dr = abs(newRow - row);
+            int dc = abs(newColumn - column);
+            int dl = abs(newLevel - level);
+
+            // Check if the movement is valid (one step in any direction)
+            if ((dr == 1 && dc == 0 && dl == 0) ||  // Move in the same level
+                (dr == 0 && dc == 1 && dl == 0) ||  // Move in the same level
+                (dr == 0 && dc == 0 && dl == 1)) {   // Move to a different level
+                // Valid move, do nothing
+            } else {
+                // Invalid move
+                continue;  // Skip this move
+            }
+
+            // Recursively explore the new move
+            if (solveMaze(maze, newRow, newColumn, newLevel)) {
+                // If a solution is found, print the current coordinates
+                std::cout << row << " " << column << " " << level << std::endl;
+                return true;
+            }
+        }
+    }
+
+    // If no solution is found, backtrack by marking the cell as visited (value 2)
+    maze.at(row, column, level) = 0;
+
+    // Updated: Return false only after all paths have been explored
+    return false;
+}
+
 int main(int argc, char* argv[]) {
-    // Write your code here
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <maze_file> <solution_file>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream mazeFile(argv[1]);
+    if (!mazeFile) {
+        std::cerr << "Error: Unable to open maze file." << std::endl;
+        return 1;
+    }
+
+    Grid maze;
+    mazeFile >> maze;
+    mazeFile.close();
+
+    // Check if the maze is solvable
+    if (solveMaze(maze, 0, 0, 0)) {
+        std::ofstream solutionFile(argv[2]);
+        solutionFile << "SOLUTION" << std::endl;
+        solutionFile << "0 0 0" << std::endl; // Add this line
+        solveMaze(maze, 0, 0, 0); // This line generates the solution path
+        solutionFile.close();
+    } else {
+        std::ofstream solutionFile(argv[2]);
+        solutionFile << "NO SOLUTION" << std::endl;
+        solutionFile.close();
+    }
+
 
     return 0;
 }
+
